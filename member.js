4c12153f9f676e932a67f385cd43f55e63fa8509
isGameFox <java.cpp.php@gmail.com>
2024-06-09 17:07:04 +0000

    Step 2

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember(){
+    var member = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "React", "Node"],
+        salary: 5000
+    };
+    return member;
+}
